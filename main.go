e5a8b29bb945792a81cc421ed26d30768a5dc172
vedaRadev <ryancarmstrong97@gmail.com>
2025-04-15 14:38:53 -0400

    add additional logging to checkhealth

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	// TODO remove io/ioutil in favor of just io
 	"io/ioutil"
+	"io"
 	"log"
 	"math"
 	"net/http"
@@ -35,13 +37,14 @@ func checkHealth(endpoint Endpoint) {
 
 	bodyBytes, err := json.Marshal(endpoint)
 	if err != nil {
+		log.Printf("%v, %v to %v, failed to marshal json: %v\n", endpoint.Name, endpoint.Method, endpoint.URL, err)
 		return
 	}
 	reqBody := bytes.NewReader(bodyBytes)
 
 	req, err := http.NewRequest(endpoint.Method, endpoint.URL, reqBody)
 	if err != nil {
-		log.Println("Error creating request:", err)
+		log.Printf("%v, %v to %v, error creating request: %v\n", endpoint.Name, endpoint.Method, endpoint.URL, err)
 		return
 	}
 
@@ -55,6 +58,19 @@ func checkHealth(endpoint Endpoint) {
 	stats[domain].Total++
 	if err == nil && resp.StatusCode >= 200 && resp.StatusCode < 300 {
 		stats[domain].Success++
+	} else {
+		if err != nil {
+			log.Printf("%v, %v to %v failed: %v\n", endpoint.Name, endpoint.Method, endpoint.URL, err)
+			return
+		}
+
+		log.Printf("%v, %v to %v, response %v. Reading body...\n", endpoint.Name, endpoint.Method, endpoint.URL, resp.StatusCode)
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Printf("failed to read body: %v\n", err)
+			return
+		}
+		log.Printf("%v, %v to %v, body: %v\n", endpoint.Name, endpoint.Method, endpoint.URL, string(bodyBytes))
 	}
 }
 
@@ -94,6 +110,7 @@ func main() {
 	}
 
 	filePath := os.Args[1]
+	// TODO ioutil is deprecated as of Go 1.16. Use os.ReadFile instead.
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Fatal("Error reading file:", err)
